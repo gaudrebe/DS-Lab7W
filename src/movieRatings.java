f97458b6297d1087f6569d2872fa08c656b98a4f
EthanGilles <ehgilles01@gmail.com>
2023-11-08 14:41:20 -0500

    Updates with commented code

--- src/movieRatings.java
+++ src/movieRatings.java
@@ -10,30 +10,33 @@ public class movieRatings {
 	
 	public static List<String> rankMovies(TreeMap<String, TreeSet<String>> map) {
 		
-		HashMap<String, Integer> tempMap = new HashMap<String, Integer>();
+		HashMap<String, Integer> tempMap = new HashMap<String, Integer>(); // make a map that will store an integer value for each movie
 		
 		for(Map.Entry<String, TreeSet<String>> entry : map.entrySet( )) {
 			TreeSet<String> set = entry.getValue();
-			for(String movie: set) {
+			for(String movie: set) { //for every movie, for every student, either update the number or give it a number
 				
 				if(tempMap.containsKey(movie)) {
-					tempMap.put(movie, tempMap.get(movie) + 1);
+					tempMap.put(movie, tempMap.get(movie) + 1); //update number
 				}
 				else {
-					tempMap.put(movie, 1);
+					tempMap.put(movie, 1); //first time we see a movie
 
 				}
 			}
 		}
 		
-		List<String> result = new ArrayList<String>();
+		List<String> result = new ArrayList<String>(); //the list to store our results
 		
 		for(Map.Entry<String, Integer> entry : tempMap.entrySet()) {
-			if(result.isEmpty())
+			if(result.isEmpty()) //add the first value
 				result.add(entry.getKey());
 			else {
-				for(int i = 0; i < result.size(); i++) {
-					if(tempMap.get(result.get(i)) <= entry.getValue() ){
+				for(int i = 0; i <= result.size(); i++) { 
+					if(i == result.size()) { //if we have reached the end of the list, add the number
+						result.add(entry.getKey());
+					}
+					else if(tempMap.get(result.get(i)) <= entry.getValue() ){ //if we encounter a number smaller than ours, add it in at that point, shifting everything down.
 						result.add(i, entry.getKey());
 					}
 				}
